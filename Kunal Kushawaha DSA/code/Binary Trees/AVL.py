7143f2ee108bdb41b320d3a8b67b19deb82681c2
Debopam Param <debopamwork@gmail.com>
2024-03-27 23:21:17 +0530

    AVL tree optimized

--- Kunal Kushawaha DSA/code/Binary Trees/AVL.py
+++ Kunal Kushawaha DSA/code/Binary Trees/AVL.py
@@ -3,22 +3,21 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+        self.height = 0  # New attribute
+
 
 class AVL:
     def __init__(self):
         self.root = None
 
-    def height(self, node) -> int:
+    def get_height(self, node) -> int:
         if node is None:
             return -1
-        left = self.height(node.left)
-        right = self.height(node.right)
-        return 1 + max(left, right)
+        return node.height
 
     def insert(self, val) -> None:
         if self.root is None:
             self.root = TreeNode(val)
-            return
         else:
             self.root = self.__insert(self.root, val)
 
@@ -29,23 +28,25 @@ class AVL:
             node.left = self.__insert(node.left, val)
         elif val > node.val:
             node.right = self.__insert(node.right, val)
+
+        node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
         return self.rotate(node)
 
     def rotate(self, node) -> TreeNode:
         # None is check is not required, because rotate is never called
         # when node is None
-        if (self.height(node.left) - self.height(node.right)) > 1:
+        if (self.get_height(node.left) - self.get_height(node.right)) > 1:
             # left heavy
-            if (self.height(node.left.left) - self.height(node.left.right)) > 0:
+            if (self.get_height(node.left.left) - self.get_height(node.left.right)) > 0:
                 # left - left heavy
                 return self.rotate_right(node)  # Along p
             else:
                 # left - right heavy
                 node.left = self.rotate_left(node.left)
                 return self.rotate_right(node)
-        elif (self.height(node.left) - self.height(node.right)) < -1:
+        elif (self.get_height(node.left) - self.get_height(node.right)) < -1:
             # right heavy
-            if (self.height(node.right.left) - self.height(node.right.right)) < 0:
+            if (self.get_height(node.right.left) - self.get_height(node.right.right)) < 0:
                 # right - right heavy
                 return self.rotate_left(node)  # Along p
             else:
@@ -56,14 +57,18 @@ class AVL:
 
     def rotate_right(self, p):
         c = p.left
-        t = c.right  # t that needs to get changed
+        t = c.right
         c.right = p
         p.left = t
+        p.height = 1 + max(self.get_height(p.left), self.get_height(p.right))
+        c.height = 1 + max(self.get_height(c.left), self.get_height(c.right))
         return c
 
     def rotate_left(self, p):
         c = p.right
-        t = c.left  # t that needs to get changed
+        t = c.left
         c.left = p
         p.right = t
+        p.height = 1 + max(self.get_height(p.left), self.get_height(p.right))
+        c.height = 1 + max(self.get_height(c.left), self.get_height(c.right))
         return c
